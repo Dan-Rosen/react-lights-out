c8a87033b3b97dc6f8c72e341ae900760302a4fd
Dan Rosen <drosen@precipart.com>
2020-05-18 20:57:32 -0400

    added winning logic

--- src/Board.js
+++ src/Board.js
@@ -47,11 +47,16 @@ class Board extends Component {
         flipCell(y - 1, x)
         flipCell(y + 1, x)
 
-        let hasWon = false
+        let hasWon = board.every((row) => row.every((cell) => !cell))
+
         this.setState({ board, hasWon })
     }
 
     render() {
+        if (this.state.hasWon) {
+            return <h1>You Won!</h1>
+        }
+
         let tblBoard = []
         for (let y = 0; y < this.props.nrows; y++) {
             let row = []
